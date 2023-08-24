8ade2a2de82fe0c3fc5e0897a9cbe3ae58e118ee
kim.kangyong <test@kim.com>
2023-08-24 10:21:02 +0800

    laravel inertia crud

---
+++ resources/js/Pages/Chirps/Index.vue
@@ -0,0 +1,32 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import Chirp from '@/Components/Chirp.vue';
+import InputError from '@/Components/InputError.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import { useForm, Head } from '@inertiajs/vue3';
+
+defineProps(['chirps']);
+
+const form = useForm({
+    message: '',
+});
+</script>
+
+<template>
+    <Head title="Chrips" />
+
+    <AuthenticatedLayout>
+        <div class="max-2-2xl mx-auto p-4 sm:p-6 lg:p-8">
+            <form @submit.prevent="form.post(route('chirps.store'), { onSuccess: () => form.reset() })">
+                <textarea v-model="form.message" placeholder="What's on your mind?"
+                    class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"></textarea>
+                <InputError :message="form.errors.message" class="mt-2" />
+                <PrimaryButton class="mt-4">Chirp</PrimaryButton>
+            </form>
+
+            <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+                <Chirp v-for="chirp in chirps" :key="chirp.id" :chirp="chirp" />
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
